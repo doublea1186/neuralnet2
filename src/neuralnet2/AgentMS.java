7853c57a69075939db4bd718c64ea0857d507c18
Ahmed Ahmed <doublea1186@gmail.com>
2019-02-11 19:01:10 -0500

    version 1.0 of integrated good and bad mines

--- src/neuralnet2/AgentMS.java
+++ src/neuralnet2/AgentMS.java
@@ -16,7 +16,9 @@ public class AgentMS {
     private double lTrack, rTrack;  //the influence rating toward turning left and turning right, used as outputs
     private double fitness;			//how well the agent is doing, quantified (for the genetic algorithm)
     private double scale; 			//the size of the agent
-    private int closestMine;		//the index in the mines list of the mine closest to the agent (used to determine inputs for the neural net)
+ //   private int closestMine;		//the index in the mines list of the mine closest to the agent (used to determine inputs for the neural net)
+    private int gIndex;
+    private int bIndex;
 
     public AgentMS() { //initialization
         Random rnd = new Random();
@@ -26,7 +28,8 @@ public class AgentMS {
         rTrack = 0.16;
         fitness = 0;
         scale = ControllerMS.SCALE;
-        closestMine = 0;
+        gIndex = 0;
+        bIndex = 0;
         position = new Point2D.Double(rnd.nextDouble() * Params.WIN_WIDTH, rnd.nextDouble() * Params.WIN_HEIGHT);
         facing = new Point2D.Double(-Math.sin(rotation), Math.cos(rotation));
     }
@@ -34,19 +37,25 @@ public class AgentMS {
     public boolean update(ArrayList<Mines> mines) {		//updates all the parameters of the sweeper, sounds fairly important
         ArrayList<Double> inputs = new ArrayList<Double>();
         //find the closest mine, figure out the direction the mine is from the sweeper's perspective by creating a unit vector
-        Point2D closestMine1 = getClosestMine(mines);
-        double shortestDistance = Point2D.distance(position.getX(), position.getY(), closestMine1.getX(), closestMine1.getY());
-        double xfacing = (position.getX() - closestMine1.getX()) / shortestDistance;
-        double yfacing = (position.getY() - closestMine1.getY()) / shortestDistance;
+        Point2D x = getClosestMine(mines);
+
+        Point2D closestGMine = mines.get(this.gIndex).getPosition();
+        double shortestGDistance = Point2D.distance(position.getX(), position.getY(), closestGMine.getX(), closestGMine.getY());
+        double gxfacing = (position.getX() - closestGMine.getX()) / shortestGDistance;
+        double gyfacing = (position.getY() - closestGMine.getY()) / shortestGDistance;
+
+        Point2D closestBMine = mines.get(this.bIndex).getPosition();
+        double shortestBDistance = Point2D.distance(position.getX(), position.getY(), closestBMine.getX(), closestBMine.getY());
+        double bxfacing = (position.getX() - closestBMine.getX()) / shortestBDistance;
+        double byfacing = (position.getY() - closestBMine.getY()) / shortestBDistance;
 
         inputs.add(facing.getX());
         inputs.add(facing.getY());
-        inputs.add(xfacing);
-        inputs.add(yfacing);
-        if(mines.get(closestMine).getType() == true)
-            inputs.add((double)0);
-        else
-            inputs.add((double)1);
+        inputs.add(gxfacing);
+        inputs.add(gyfacing);
+        inputs.add(bxfacing);
+        inputs.add(byfacing);
+
 
         ArrayList<Double> output = brain.Update(inputs);
 
@@ -73,24 +82,56 @@ public class AgentMS {
         return true;
     }
 
-    public Point2D getClosestMine(ArrayList<Mines> mines) { //finds the mine closest to the sweeper
-        double closestSoFar = 99999999;
-        Point2D closestObject = new Point2D.Double(0,0);
+//    public Point2D getClosestMine(ArrayList<Mines> mines) { //finds the mine closest to the sweeper
+//        double closestSoFar1 = 99999999;
+//        double closestSoFar2 = 99999999;
+//        Point2D closestObject = new Point2D.Double(0,0);
+//        double length;
+//        for (int i = 0; i < mines.size(); i++) {
+//            length = Point2D.distanceSq(mines.get(i).getPosition().getX(), mines.get(i).getPosition().getY(), position.getX(), position.getY());
+//            if (length < closestSoFar) {
+//                closestSoFar = length;
+//                closestObject = (mines.get(i).getPosition());
+//                if(mines.get(closestMine).getType())
+//                gIndex = i;
+//                else
+//                    bIndex = i;
+//            }
+//        }
+//        return closestObject;
+//    }
+    public Point2D getClosestMine(ArrayList<Mines> mines) {
         double length;
-        for (int i = 0; i < mines.size(); i++) {
+        double length1 = 999999;
+        double length2 = 999999;
+        Point2D closestObject = new Point2D.Double(0,0);
+
+        for (int i = 0; i < mines.size(); i++){
             length = Point2D.distanceSq(mines.get(i).getPosition().getX(), mines.get(i).getPosition().getY(), position.getX(), position.getY());
-            if (length < closestSoFar) {
-                closestSoFar = length;
-                closestObject = (mines.get(i).getPosition());
-                closestMine = i;
+            if (mines.get(i).getType()){
+                if(length < length1){
+                    length1 = length;
+                    closestObject = (mines.get(i).getPosition());
+                    gIndex = i;
+                }
+            }
+            else{
+                if(length < length2){
+                    length2 = length;
+                    bIndex = i;
+                    if (length2 < length1)
+                        closestObject = (mines.get(i).getPosition());
+                }
             }
         }
         return closestObject;
     }
-
     public int checkForMine(ArrayList<Mines> mines, double size) { //has the sweeper actually swept up the closest mine to it this tick?
-        if (Point2D.distance(position.getX(), position.getY(), mines.get(closestMine).getPosition().getX(), mines.get(closestMine).getPosition().getY()) < (size + scale / 2)) {
-            return closestMine;
+        if (Point2D.distance(position.getX(), position.getY(), mines.get(gIndex).getPosition().getX(), mines.get(gIndex).getPosition().getY()) < (size + scale / 2)) {
+            return gIndex;
+        }
+        else if(Point2D.distance(position.getX(), position.getY(), mines.get(bIndex).getPosition().getX(), mines.get(bIndex).getPosition().getY()) < (size + scale / 2)) {
+            return bIndex;
         }
         return -1;
     }
@@ -134,5 +175,8 @@ public class AgentMS {
     public double getFitness() { return fitness; }
     public void putWeights(ArrayList<Double> w) { brain.replaceWeights(w); }
     public int getNumberOfWeights() { return brain.getNumberOfWeights(); }
-    public int getClosestMine() {return closestMine; }
+    public int getGIndex(){return gIndex;}
+    public int getBIndex() {return bIndex;}
+    public void setGIndex(int x){gIndex = x;}
+    public void setBIndex(int x){bIndex = x;}
 }
